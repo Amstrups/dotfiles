b57f61a83aa4b110a90b6be37673e6c8ae2702d0
Christian Amstrup Petersen <christian.amstrup@hotmail.com>
2024-01-16 15:07:58 +0100

    add orange bg to distinguishing between windows

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,1 +1,16 @@
-require('lualine').setup()
+local colors = { 
+  active_orange =    '#d94e18',
+  inactive_orange =  '#70270a',
+  inactive_fg_grey = '#e0e0e0',
+} 
+local custom_catppuccin = require'lualine.themes.catppuccin'
+
+custom_catppuccin.inactive.c.bg = colors.inactive_orange
+custom_catppuccin.inactive.c.fg = colors.inactive_fg_grey
+custom_catppuccin.normal.c.bg = colors.active_orange
+custom_catppuccin.normal.c.fg = colors.inactive_fg_grey
+
+
+require('lualine').setup { 
+  options = { theme = custom_catppuccin } 
+}
