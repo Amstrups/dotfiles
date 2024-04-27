8daa147abaa8cdbda63f9948515446d1eb7c7409
Christian Amstrup Petersen <christian.amstrup@hotmail.com>
2024-04-27 15:11:49 +0200

    dont actually remember

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,3 +2,24 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>rf', function()
+  print("NYI")
+  builtin.lsp_references()
+end, {})
+vim.keymap.set('n', '<leader>ts', function()
+  print("tst")
+  builtin.lsp_outgoing_calls()
+end, {})
+
+
+--for key,value in pairs(builtin) do
+--    print("found member " .. key);
+--end
+
+require('telescope').setup {
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    }
+  }
+}
