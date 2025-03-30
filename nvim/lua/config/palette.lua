bcced7acdcf95a161e7ec17a1b955c0799827fcc
Christian Amstrup Petersen <christian.amstrup@hotmail.com>
2025-03-30 13:03:56 +0200

    feat: include old feature of distinguished lualines

---
+++ nvim/lua/config/palette.lua
@@ -0,0 +1,9 @@
+local M = {
+	lualine = {
+		active_orange = '#d94e18',
+		inactive_orange = '#70270a',
+		inactive_fg_grey = '#e0e0e0',
+	}
+}
+
+return M
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,7 +2,26 @@ return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	lazy = vim.fn.argc(-1) == 0,
-	config = function ()
-		require("lualine").setup()
+	config = function()
+		local custom_catppuccin = require 'lualine.themes.catppuccin'
+		local lualine = require('config.palette').lualine
+
+		custom_catppuccin.inactive.c.bg = lualine.inactive_orange
+		custom_catppuccin.inactive.c.fg = lualine.inactive_fg_grey
+		custom_catppuccin.normal.c.bg = lualine.active_orange
+		custom_catppuccin.normal.c.fg = lualine.inactive_fg_grey
+
+
+		require('lualine').setup {
+			options = { theme = custom_catppuccin },
+			sections = {
+				lualine_c = {
+					{
+						'filename',
+						path = 1,
+					}
+				}
+			}
+		}
 	end,
 }
